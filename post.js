09caff6e54339dc8796aa8c085b183b55365e836
Louis-Guillaume MORAND <lgm@lgmorand.com>
2021-05-10 21:40:13 +0200

    Update post.js

--- post.js
+++ post.js
@@ -3,5 +3,5 @@ console.log(`I am the post-script`);
 
 const isPost = Boolean(process.env['STATE_isPost']);
 
-console.log(process.env['STATE_isPost']];
+console.log(process.env['STATE_isPost']);
 console.log(isPost);
